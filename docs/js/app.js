428298d83f9330f0bd67a01b399cc5cd11e932da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-12 19:56:25 +0000

    Deploy joymon/prayerbook to github.com/joymon/prayerbook.git:master

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,5 +1,16 @@
-
-var app = angular.module('prayerBook', ['ui.bootstrap']);
-app.service('GAAPI', ['$http', GAAPI]);
-app.controller('MainCtrl', ['$scope', '$http', '$sce', '$modal','GAAPI', MainCtrl]);
-app.controller('DevCreditsCtrl', DevCreditsCtrl);
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./service-worker.js").then(function() {
+    console.log("Service Worker Registered");
+  });
+}
+var app = angular.module("prayerBook", ["ui.bootstrap"]);
+app.service("GAAPI", ["$http", GAAPI]);
+app.controller("MainCtrl", [
+  "$scope",
+  "$http",
+  "$sce",
+  "$modal",
+  "GAAPI",
+  MainCtrl
+]);
+app.controller("DevCreditsCtrl", DevCreditsCtrl);
