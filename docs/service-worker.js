dfd49bc49a4a0553fbb01143e88b33c5f1b7d115
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-20 01:28:43 +0000

    Deploy joymon/prayerbook to github.com/joymon/prayerbook.git:master

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 //Below version will be patched by the \build with latest build number.
-var CACHE_NAME = "pb-site-cache-v0.0.1300";
+var CACHE_NAME = "pb-site-cache-v0.0.1301";
 
 var urlsToCache = [
   "/",
